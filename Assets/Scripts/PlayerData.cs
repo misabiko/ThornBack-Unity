ce01238b3a88dfab3c8593318c9077960d7d60b9
misabiko <misabiko@gmail.com>
2020-04-07 15:49:48 -0400

    Added basic character controller

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject {
+	public float speed = 1f;
+	public float sprintSpeed = 2f;
+	public float accel = 100f;
+	public float jumpForce = 3f;
+
+	public float camSensitivityX = 1f;
+	public float camSensitivityY = 1f;
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour {
+	PlayerData data;
+	new Rigidbody rigidbody;
+	Transform camTransform;
+	Vector3 moveInput;
+	Vector3 moveDirection;
+	bool sprinting;
+
+	void Awake() {
+		data = GetComponent<Player>()?.data;
+		rigidbody = GetComponent<Rigidbody>();
+
+		Camera cam = Camera.main;
+		if (cam != null)
+			camTransform = cam.transform;
+	}
+
+	void Update() => moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+	void FixedUpdate() {
+		Vector3 force = moveDirection * data.accel;
+		
+		float maxSpeed;
+		maxSpeed = sprinting ? data.sprintSpeed : data.speed;
+
+		rigidbody.AddForce(force);
+		ClampVelocityXZ(maxSpeed);
+	}
+	
+	// ReSharper disable once InconsistentNaming
+	void ClampVelocityXZ(float maxSpeed) {
+		Vector3 flatVel = rigidbody.velocity;
+		flatVel.y = 0;
+
+		if (flatVel.magnitude > maxSpeed)
+			flatVel = flatVel.normalized * maxSpeed;
+		flatVel.y = rigidbody.velocity.y;
+		rigidbody.velocity = flatVel;
+	}
+	
+	public void OnMove(InputAction.CallbackContext ctx) {
+		Vector2 input = ctx.ReadValue<Vector2>();
+		float magnitude = input.magnitude;
+		if (magnitude > 1f)
+			input.Normalize();
+
+		moveInput = input;
+	}
+
+	public void OnSprint(InputAction.CallbackContext ctx) => sprinting = ctx.ReadValueAsButton();
+
+	public void OnJump(InputAction.CallbackContext ctx) {
+		if (!ctx.ReadValueAsButton() || !IsGrounded()) return;
+		
+		rigidbody.AddForce(data.jumpForce * Vector3.up, ForceMode.Impulse);
+	}
+
+	bool IsGrounded() => true;
+
+	public void OnLook(InputAction.CallbackContext ctx) {
+		var lookInput = ctx.ReadValue<Vector2>();
+		
+		transform.Rotate(Vector3.up, lookInput.x * data.camSensitivityX);
+		camTransform.Rotate(Vector3.right, -lookInput.y * data.camSensitivityY);
+	}
+}

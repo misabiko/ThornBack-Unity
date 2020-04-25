b071c07d0d48b609008a100f54fc4de82f917d5c
misabiko <misabiko@gmail.com>
2020-04-25 07:09:02 -0400

    PlayerTag → PlayerAuthoring + Added playerData to PlayerMovementSystem

---
+++ Assets/Scripts/PlayerAuthoring.cs
@@ -0,0 +1,20 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+public class PlayerAuthoring : MonoBehaviour, IConvertGameObjectToEntity {
+	public PlayerData playerData;
+
+
+	public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem) {
+		dstManager.SetName(entity, "Player");
+		dstManager.AddSharedComponentData(entity, new PlayerTransform{value = transform});
+		
+		dstManager.AddComponent<PlayerChunkCoord>(entity);
+		var playerQuery = dstManager.CreateEntityQuery(typeof(PlayerChunkCoord));
+		playerQuery.SetSingleton(new PlayerChunkCoord {chunkX = 0, chunkY = 0});
+
+		dstManager.World.GetExistingSystem<PlayerMovementSystem>().playerData = playerData;
+	}
+}

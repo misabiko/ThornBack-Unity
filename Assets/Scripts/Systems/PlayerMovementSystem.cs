689c666954924214620130b9ed74fea91df340b4
misabiko <misabiko@gmail.com>
2020-04-25 07:16:08 -0400

    Added friction to player movement

--- Assets/Scripts/Systems/PlayerMovementSystem.cs
+++ Assets/Scripts/Systems/PlayerMovementSystem.cs
@@ -1,14 +1,35 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Physics;
+using UnityEngine;
 
 public class PlayerMovementSystem : SystemBase {
+	public PlayerData playerData;
 	
-	
-	protected override void OnUpdate() =>
+	protected override void OnUpdate() {
+		//TODO Make that a one off
 		Entities
-			.WithAll<PlayerComponent>()
+			.WithAll<PlayerChunkCoord>()
 			.ForEach(
 				(ref PhysicsMass mass) => mass.InverseInertia = float3.zero
 			).ScheduleParallel();
+
+		float deccel = playerData.deccel;
+		
+		Entities
+			.WithAll<PlayerChunkCoord>()
+			.ForEach(
+				(ref PhysicsVelocity velocity) => {
+					float3 flatVel = velocity.Linear;
+					flatVel.y = 0;
+
+					if (math.length(flatVel) > 0.2f)
+						velocity.Linear -= math.normalize(flatVel) * deccel;
+					else {
+						velocity.Linear.x = 0;
+						velocity.Linear.z = 0;
+					}
+				}
+			).ScheduleParallel();
+	}
 }
